1ffbe2089b4d779550eedcda78658f5751ff6442
sv-widegreen <svenja@wide.green>
2020-06-19 18:19:04 +0200

    Add product pictures and image styling

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -7,6 +7,7 @@ export default function ProductListItem({ list }) {
         return (
           <li key={product + index} className="product-card">
             <h2>{product.productName}</h2>
+            <img src={product.productPicture} alt={product.productName} />
             <p>Preis: {product.productPrice} €</p>
             <p>{product.productDescription}</p>
           </li>
