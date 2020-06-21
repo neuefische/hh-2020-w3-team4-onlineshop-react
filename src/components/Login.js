6497136b09064a50d8b6eb04638f5c36e72eda13
Miriam <miriam.haenle@gmail.com>
2020-06-21 22:00:13 +0200

    Add logout functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,25 +26,30 @@ export default function Login() {
 
   return (
     <section className="flex">
-      <h2>Already have an account?</h2>
       {isLoggedIn === false ? (
-        <form onSubmit={submitHandlerLogin} className="form">
-          {' '}
-          <label className="p-10">
-            Insert your email here:{' '}
-            <input
-              type="text"
-              value={email}
-              className="input"
-              onChange={(event) => setEmail(event.target.value)}
-            ></input>
-          </label>
-          <button className="button p-10">Login</button>
-        </form>
+        <>
+          <h2>Already have an account?</h2>
+          <form onSubmit={submitHandlerLogin} className="form">
+            {' '}
+            <label className="p-10">
+              Insert your email here:{' '}
+              <input
+                type="text"
+                value={email}
+                className="input"
+                onChange={(event) => setEmail(event.target.value)}
+              ></input>
+            </label>
+            <button className="button p-10">Login</button>
+          </form>
+        </>
       ) : (
         <>
           <h2>Welcome {user.customerName}</h2>
           <p>You have successfully logged in!</p>
+          <button className="button p-10" onClick={logOut}>
+            Logout
+          </button>
         </>
       )}
     </section>
@@ -60,5 +65,10 @@ export default function Login() {
         setUser(response.data)
         setIsLoggedIn(true)
       })
+  }
+
+  function logOut(event) {
+    event.preventDefault()
+    setIsLoggedIn(false)
   }
 }
