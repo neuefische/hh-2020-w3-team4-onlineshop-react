0f5fcccc40ae8d74425c421b0930bddfe7149708
sinafischer <sina.h.fischer@gmail.com>
2020-06-26 11:45:01 +0200

    Add navigation comopnent

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import fishingBoat from '../assets/boat.png'
+import fishingRod from '../assets/fishing-rod.png'
+import fisherman from '../assets/fisherman.png'
+import fishernet from '../assets/trap.png'
+
+export default function () {
+  return (
+    <nav>
+      <ul className="flex-row">
+        <li className="nav-item flex">
+          <NavLink exact to="/">
+            {' '}
+            <img className="nav-img" src={fishingBoat} alt="fishingboat" /> Home
+          </NavLink>
+        </li>
+        <li className="nav-item flex">
+          <NavLink to="/products">
+            <img className="nav-img" src={fishingRod} alt="fishingrod" />
+            Products
+          </NavLink>
+        </li>
+        <li className="nav-item flex">
+          <NavLink to="/users">
+            {' '}
+            <img className="nav-img" src={fisherman} alt="fisherman" />
+            Users
+          </NavLink>
+        </li>
+        <li className="nav-item flex">
+          <NavLink to="/orders">
+            <img className="nav-img" src={fishernet} alt="fishernet" />
+            Orders
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
